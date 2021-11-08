5b1d73e31fa248ff02b61a1d87537a2745a661dd
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-11-08 15:54:46 +0700

    Update Game Over

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Events : MonoBehaviour
+{
+    public void ReplayGame(){
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
