79e3da4f4a5ba73df91885ca966f56efbdce10de
FaridRey <89561572+faridfr45@users.noreply.github.com>
2021-12-11 15:47:09 +0700

    Fix double score

---
+++ Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+
+    [Header("Double Score")]
+    public PowerUpData doubleScore = new PowerUpData();
+
+    private void Update() {
+        if(doubleScore.isActive){
+            doubleScore.timer -= Time.deltaTime;
+            
+            if(doubleScore.timer <= 0){
+                doubleScore.isActive = false;
+                GameManager.Instance.scoreRate /= 2;
+                doubleScore.timer = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.transform.tag == "DoubleScore"){
+            other.gameObject.SetActive(false);
+            DoubleScore();
+        }
+    }
+
+    private void DoubleScore(){
+        if(!doubleScore.isActive){
+            GameManager.Instance.scoreRate *= 2;
+            doubleScore.isActive = true;
+        }
+        doubleScore.timer += doubleScore.duration;
+
+        if(doubleScore.timer > doubleScore.duration)
+            doubleScore.timer = doubleScore.duration;
+    }
+
+    [System.Serializable]
+    public class PowerUpData
+    {
+        public float duration = 8f;
+        public float timer;
+        public bool isActive;
+    }
+}
